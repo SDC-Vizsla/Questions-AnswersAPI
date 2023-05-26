899f5a8855c31d1984c076ae0faec16c285db5fa
Koen Arnold <kostarnold@gmail.com>
2023-05-26 17:25:14 -0400

    Wrote the get questions and get answers api routes

--- server/dbSetup.js
+++ server/dbSetup.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
 const path = require('path');
+const fs = require('fs');
+const csv = require('csv-parser')
 require('dotenv').config()
 
 
@@ -15,15 +17,37 @@ const client = new Pool ({
 const setupDB = async function () {
 
   const createSchemaAndTables = async function () {
-    client.query('CREATE SCHEMA IF NOT EXISTS QuestionsAnswersSchema')
-    await client.query('CREATE TABLE IF NOT EXISTS QuestionsAnswersSchema.answers (id BIGINT, question_id BIGINT, body VARCHAR, date_written BIGINT, answerer_name VARCHAR, answerer_email VARCHAR, reported BIGINT, helpfulness BIGINT)');
-    await client.query('CREATE TABLE IF NOT EXISTS QuestionsAnswersSchema.questions (id BIGINT, product_id BIGINT, body VARCHAR, date_written BIGINT, asker_name VARCHAR, asker_email VARCHAR, reported BIGINT, helpfulness BIGINT)');
-    await client.query('CREATE TABLE IF NOT EXISTS QuestionsAnswersSchema.answers_photos (id BIGINT, answer_id BIGINT, url VARCHAR)');
+    await client.query(`CREATE SCHEMA IF NOT EXISTS ${process.env.SCHEMA}`)
+
+    await client.query(`CREATE TABLE IF NOT EXISTS ${process.env.SCHEMA}.questions
+    (id BIGINT PRIMARY KEY,
+    product_id BIGINT,
+    body VARCHAR (1000),
+    date_written BIGINT,
+    asker_name VARCHAR,
+    asker_email VARCHAR,
+    reported INTEGER DEFAULT 0,
+    helpfulness INTEGER)`);
+
+    await client.query(`CREATE TABLE IF NOT EXISTS ${process.env.SCHEMA}.answers
+    (id BIGINT PRIMARY KEY,
+    question_id BIGINT,
+    body VARCHAR (1000),
+    date_written BIGINT,
+    answerer_name VARCHAR,
+    answerer_email VARCHAR,
+    reported INTEGER,
+    helpfulness INTEGER)`);
+
+    await client.query(`CREATE TABLE IF NOT EXISTS ${process.env.SCHEMA}.answers_photos
+    (id BIGINT PRIMARY KEY,
+    answer_id BIGINT,
+    url VARCHAR)`);
   }
 
   const loadQuestionData = async function () {
     const questionsPath = '/tmp/sample_data/questions.csv'
-    await client.query(`Copy QuestionsAnswersSchema.questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpfulness) FROM '${questionsPath}' DELIMITER ',' CSV HEADER`, (err, callback)=>{if (err) {
+    await client.query(`COPY ${process.env.SCHEMA}.questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpfulness) FROM '${questionsPath}' DELIMITER ',' CSV HEADER`, (err, callback)=>{if (err) {
       console.log('error: ', err)
     } else {
       console.log('data loaded to questions table')
@@ -31,7 +55,7 @@ const setupDB = async function () {
   }
   const loadAnswerData = async function () {
     const answersPath = '/tmp/sample_data/answers.csv'
-    await client.query(`Copy QuestionsAnswersSchema.answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpfulness) FROM '${answersPath}' DELIMITER ',' CSV HEADER`, (err, callback)=>{if (err) {
+    await client.query(`COPY ${process.env.SCHEMA}.answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpfulness) FROM '${answersPath}' DELIMITER ',' CSV HEADER`, (err, callback)=>{if (err) {
       console.log('error: ', err)
     } else {
       console.log('data loaded to answers table')
@@ -39,7 +63,7 @@ const setupDB = async function () {
   }
   const loadAnswersPhotosData = async function () {
     const answers_photosPath = '/tmp/sample_data/answers_photos.csv'
-    await client.query(`Copy QuestionsAnswersSchema.answers_photos(id, answer_id, url) FROM '${answers_photosPath}' DELIMITER ',' CSV HEADER`, (err, callback)=>{if (err) {
+    await client.query(`COPY ${process.env.SCHEMA}.answers_photos(id, answer_id, url) FROM '${answers_photosPath}' DELIMITER ',' CSV HEADER`, (err, callback)=>{if (err) {
       console.log('error: ', err)
     } else {
       console.log('data loaded to answers_photos table')
@@ -50,21 +74,23 @@ const setupDB = async function () {
   }
 
 
-  createSchemaAndTables().then(async ()=>{
-    var isQTableEmpty = await client.query('SELECT * FROM questionsanswersschema.questions WHERE id = 1')
-    var isATableEmpty = await client.query('SELECT * FROM questionsanswersschema.answers WHERE id = 1')
-    var isAPTableEmpty = await client.query('SELECT * FROM questionsanswersschema.answers_photos WHERE id = 1')
+    createSchemaAndTables().then(async ()=>{
+      var isQTableEmpty = await client.query(`SELECT * FROM ${process.env.SCHEMA}.questions WHERE id = 1`)
+      var isATableEmpty = await client.query(`SELECT * FROM ${process.env.SCHEMA}.answers WHERE id = 1`)
+      var isAPTableEmpty = await client.query(`SELECT * FROM ${process.env.SCHEMA}.answers_photos WHERE id = 1`)
 
-    if (isQTableEmpty.rows.length === 0 && isATableEmpty.rows.length === 0 && isAPTableEmpty.rows.length === 0) {
-      try {
-      await loadQuestionData();
-      await loadAnswerData();
-      await loadAnswersPhotosData();
-      } catch (error) {
-        console.log('error: ', error)
+      if (isQTableEmpty.rows.length === 0 && isATableEmpty.rows.length === 0 && isAPTableEmpty.rows.length === 0) {
+        try {
+        await loadQuestionData();
+        await loadAnswerData();
+        await loadAnswersPhotosData();
+        } catch (error) {
+          console.log('error: ', error)
+        }
       }
-    }
-  })
+    })
 }
+
+
 
 setupDB()
