22e4b8578a4e9bfd9aeb0421e8a480d276f57aa8
Koen Arnold <kostarnold@gmail.com>
2023-05-31 15:15:37 -0400

    Routes Complete

---
+++ server/dbSetupAWS.js
@@ -0,0 +1,104 @@
+const { Pool } = require('pg');
+const path = require('path');
+const fs = require('fs');
+const csv = require('csv-parser')
+require('dotenv').config()
+
+
+const client = new Pool ({
+  user: process.env.USERNAME,
+  host: process.env.HOST,
+  database: process.env.DB_NAME,
+  password: process.env.PASSWORD,
+  port: process.env.PORT
+});
+
+
+const setupDB = async function () {
+
+  const createSchemaAndTables = async function () {
+    await client.query(`CREATE SCHEMA IF NOT EXISTS ${process.env.SCHEMA}`)
+
+    await client.query(`CREATE TABLE IF NOT EXISTS ${process.env.SCHEMA}.questions
+    (question_id SERIAL PRIMARY KEY,
+    product_id INTEGER,
+    body VARCHAR (1000),
+    date_written BIGINT,
+    asker_name VARCHAR,
+    asker_email VARCHAR,
+    reported INTEGER DEFAULT 0,
+    helpfulness INTEGER)`);
+
+    await client.query(`CREATE TABLE IF NOT EXISTS ${process.env.SCHEMA}.answers
+    (answer_id SERIAL PRIMARY KEY,
+    question_id INTEGER,
+    body VARCHAR (1000),
+    date_written BIGINT,
+    answerer_name VARCHAR,
+    answerer_email VARCHAR,
+    reported INTEGER,
+    helpfulness INTEGER,
+    FOREIGN KEY (question_id) REFERENCES ${process.env.SCHEMA}.questions (question_id))`);
+
+    await client.query(`CREATE TABLE IF NOT EXISTS ${process.env.SCHEMA}.answers_photos
+    (photo_id SERIAL PRIMARY KEY,
+    answer_id BIGINT,
+    url VARCHAR)`);
+  }
+
+  const createIndexes = async function () {
+    await client.query(`CREATE INDEX product_id_index ON ${process.env.SCHEMA}.questions (product_id)`);
+    await client.query(`CREATE INDEX question_id_index ON ${process.env.SCHEMA}.answers (question_id)`);
+    await client.query(`CREATE INDEX answer_id_index ON ${process.env.SCHEMA}.answers_photos (answer_id)`);
+  }
+
+  const loadQuestionData = async function () {
+    const questionsPath = '/data/questions.csv'
+    await client.query(`COPY ${process.env.SCHEMA}.questions(question_id, product_id, body, date_written, asker_name, asker_email, reported, helpfulness) FROM '${questionsPath}' DELIMITER ',' CSV HEADER`, (err, callback)=>{if (err) {
+      console.log('error: ', err)
+    } else {
+      console.log('data loaded to questions table')
+    }})
+  }
+  const loadAnswerData = async function () {
+    const answersPath = '/data/answers.csv'
+    await client.query(`COPY ${process.env.SCHEMA}.answers(answer_id, question_id, body, date_written, answerer_name, answerer_email, reported, helpfulness) FROM '${answersPath}' DELIMITER ',' CSV HEADER`, (err, callback)=>{if (err) {
+      console.log('error: ', err)
+    } else {
+      console.log('data loaded to answers table')
+    }})
+  }
+  const loadAnswersPhotosData = async function () {
+    const answers_photosPath = '/data/answers_photos.csv'
+    await client.query(`COPY ${process.env.SCHEMA}.answers_photos(photo_id, answer_id, url) FROM '${answers_photosPath}' DELIMITER ',' CSV HEADER`, (err, callback)=>{if (err) {
+      console.log('error: ', err)
+    } else {
+      console.log('data loaded to answers_photos table')
+      return;
+      // client.release()
+      // pool.end()
+//! ^^ FIGURE OUT THE DIFFERENCE BETWEEN THESE TWO ^^
+    }})
+  }
+
+
+    createSchemaAndTables().then(()=>{createIndexes()}).then(async ()=>{
+      var isQTableEmpty = await client.query(`SELECT * FROM ${process.env.SCHEMA}.questions LIMIT 1`)
+      var isATableEmpty = await client.query(`SELECT * FROM ${process.env.SCHEMA}.answers LIMIT 1`)
+      var isAPTableEmpty = await client.query(`SELECT * FROM ${process.env.SCHEMA}.answers_photos LIMIT 1`)
+
+      if (isQTableEmpty.rows.length === 0 && isATableEmpty.rows.length === 0 && isAPTableEmpty.rows.length === 0) {
+        try {
+        await loadQuestionData().then(loadAnswerData).then(loadAnswersPhotosData)
+        } catch (error) {
+          console.log('error: ', error)
+        }
+      }
+    })
+}
+
+
+
+setupDB()
+
+
